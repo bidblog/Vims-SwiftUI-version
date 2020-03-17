690b664607a734f3d0e2b82bfcb5435bbdc50c7a
Henrik Gregersen <henrik@bidblog.dk>
2020-03-17 21:11:01 +0100

    New Models

---
+++ Vims/Models/PickList.swift
@@ -0,0 +1,29 @@
+//
+//  PickList.swift
+//  Vims
+//
+//  Created by Henrik Gregersen on 17/03/2020.
+//  Copyright © 2020 bidblog. All rights reserved.
+//
+
+import SwiftUI
+
+struct PickList: Identifiable {
+    public let id = UUID()
+    public let salesOrder : SalesOrder
+    public var toBePicked = 0
+    
+    init(salesOrder: SalesOrder, toBePicked:Int) {
+        self.salesOrder = salesOrder
+        self.toBePicked = toBePicked
+    }
+}
+
+extension PickList {
+    static let demoData = [
+        PickList(salesOrder: SalesOrder(id: "4711"), toBePicked: 11),
+        PickList(salesOrder: SalesOrder(id: "4712"), toBePicked: 4),
+        PickList(salesOrder: SalesOrder(id: "4713"), toBePicked: 300),
+        PickList(salesOrder: SalesOrder(id: "4714"), toBePicked: 1),
+    ]
+}
---
+++ Vims/Models/SalesOrder.swift
@@ -0,0 +1,15 @@
+//
+//  SalesOrder.swift
+//  Vims
+//
+//  Created by Henrik Gregersen on 17/03/2020.
+//  Copyright © 2020 bidblog. All rights reserved.
+//
+
+import SwiftUI
+
+// Not expecting to show a list of salesorders
+// Not implementing Identifiable
+struct SalesOrder {
+    var id: String
+}
